f251c56826b90182900a031ab814e7ef9f5182db
Samir <samir_reaz@yahoo.com>
2021-09-20 23:12:04 +0600

    project structure complete

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFFA0A2A4);
-const kPrimaryLightColor = Color(0xFFFFECDF);
+/*const kPrimaryLightColor = Color(0xFFFFECDF);
 const kPrimaryGradientColor = LinearGradient(
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
-    colors: [Color(0xFFFFA53E), Color(0xFFFF7643)]);
+    colors: [Color(0xFFFFA53E), Color(0xFFFF7643)]);*/
 const kSecondaryColor = Color(0xFF000000);
 const kTextColorBlack = Color(0xFF000000);
 const kTextColorWhite = Color(0xFFFFFFFF);
